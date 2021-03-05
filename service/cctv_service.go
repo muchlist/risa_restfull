adbc8e20e1965f9adaf2465b599cf3127e9f2ee2
muchlist <whois.muchlis@gmail.com>
2021-03-05 22:07:49 +0800

    Cctv service use goroutine

--- service/cctv_service.go
+++ service/cctv_service.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"errors"
+	"fmt"
+	"github.com/muchlist/erru_utils_go/logger"
 	"github.com/muchlist/erru_utils_go/rest_err"
 	"github.com/muchlist/risa_restfull/constants/category"
 	"github.com/muchlist/risa_restfull/constants/enum"
@@ -11,6 +14,7 @@ import (
 	"github.com/muchlist/risa_restfull/utils/mjwt"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -55,53 +59,96 @@ func (c *cctvService) InsertCctv(user mjwt.CustomClaim, input dto.CctvRequest) (
 		input.IP = "0.0.0.0"
 	}
 
-	// Filling data
-	timeNow := time.Now().Unix()
-	data := dto.Cctv{
-		CreatedAt:       timeNow,
-		CreatedBy:       user.Name,
-		CreatedByID:     user.Identity,
-		UpdatedAt:       timeNow,
-		UpdatedBy:       user.Name,
-		UpdatedByID:     user.Identity,
-		Branch:          user.Branch,
-		ID:              idGenerated,
-		Name:            input.Name,
-		IP:              input.IP,
-		InventoryNumber: input.InventoryNumber,
-		Location:        input.Location,
-		LocationLat:     input.LocationLat,
-		LocationLon:     input.LocationLon,
-		Date:            input.Date,
-		Tag:             input.Tag,
-		Image:           "", // image empty saat inisialisasi
-		Brand:           input.Brand,
-		Type:            input.Type,
-		Note:            input.Note,
+	// kembalian dari golang channel
+	type result struct {
+		id  *string
+		err rest_err.APIError
 	}
 
-	//DB
-	insertedID, err := c.daoC.InsertCctv(data)
-	if err != nil {
-		return nil, err
+	resultChan := make(chan result, 2)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	insertToCctv := func() {
+		defer wg.Done()
+		// Filling data
+		timeNow := time.Now().Unix()
+		data := dto.Cctv{
+			CreatedAt:       timeNow,
+			CreatedBy:       user.Name,
+			CreatedByID:     user.Identity,
+			UpdatedAt:       timeNow,
+			UpdatedBy:       user.Name,
+			UpdatedByID:     user.Identity,
+			Branch:          user.Branch,
+			ID:              idGenerated,
+			Name:            input.Name,
+			IP:              input.IP,
+			InventoryNumber: input.InventoryNumber,
+			Location:        input.Location,
+			LocationLat:     input.LocationLat,
+			LocationLon:     input.LocationLon,
+			Date:            input.Date,
+			Tag:             input.Tag,
+			Image:           "", // image empty saat inisialisasi
+			Brand:           input.Brand,
+			Type:            input.Type,
+			Note:            input.Note,
+		}
+
+		//DB
+		insertedID, err := c.daoC.InsertCctv(data)
+
+		resultChan <- result{
+			id:  insertedID,
+			err: err,
+		}
 	}
 
-	// Menambahkan juga General Unit dengan FilterID yang sama
-	//DB
-	_, err = c.daoG.InsertUnit(
-		dto.GenUnit{
-			ID:       idGenerated.Hex(),
-			Category: category.Cctv,
-			Name:     input.Name,
-			IP:       input.IP,
-			Branch:   user.Branch,
-		})
-	if err != nil {
-		return nil, err
+	insertToGenUnit := func() {
+		defer wg.Done()
+		// Menambahkan juga General Unit dengan ID yang sama
+		//DB
+		insertedID, err := c.daoG.InsertUnit(
+			dto.GenUnit{
+				ID:       idGenerated.Hex(),
+				Category: category.Cctv,
+				Name:     input.Name,
+				IP:       input.IP,
+				Branch:   user.Branch,
+			})
+		resultChan <- result{
+			id:  insertedID,
+			err: err,
+		}
 	}
 
-	return insertedID, nil
+	go insertToCctv()
+	go insertToGenUnit()
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	var resultID *string
+	var errString string
+	for v := range resultChan {
+		if v.err != nil {
+			errString = errString + v.err.Message() + ". "
+		}
+		if resultID == nil {
+			resultID = v.id
+		}
+	}
+
+	if errString != "" {
+		return nil, rest_err.NewBadRequestError(errString)
+	}
+
+	return resultID, nil
 }
+
 func (c *cctvService) EditCctv(user mjwt.CustomClaim, cctvID string, input dto.CctvEditRequest) (*dto.Cctv, rest_err.APIError) {
 
 	oid, errT := primitive.ObjectIDFromHex(cctvID)
@@ -146,44 +193,75 @@ func (c *cctvService) EditCctv(user mjwt.CustomClaim, cctvID string, input dto.C
 		return nil, err
 	}
 
-	// IMROVEMENT jadikan goroutine EditUnit dan InsertHistory
-	//DB
-	_, err = c.daoG.EditUnit(cctvID, dto.GenUnitEditRequest{
-		Category: category.Cctv,
-		Name:     cctvEdited.Name,
-		IP:       cctvEdited.IP,
-		Branch:   cctvEdited.Branch,
-	})
-	if err != nil {
-		return nil, err
+	errChan := make(chan rest_err.APIError, 2)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	editUnit := func() {
+		defer wg.Done()
+		//DB
+		_, err = c.daoG.EditUnit(cctvID, dto.GenUnitEditRequest{
+			Category: category.Cctv,
+			Name:     cctvEdited.Name,
+			IP:       cctvEdited.IP,
+			Branch:   cctvEdited.Branch,
+		})
+		errChan <- err
 	}
 
-	//DB
-	_, err = c.daoH.InsertHistory(
-		dto.History{
-			ID:             primitive.NewObjectID(),
-			CreatedAt:      timeNow,
-			CreatedBy:      user.Name,
-			CreatedByID:    user.Identity,
-			UpdatedAt:      timeNow,
-			UpdatedBy:      user.Name,
-			UpdatedByID:    user.Identity,
-			Category:       category.Cctv,
-			Branch:         user.Branch,
-			ParentID:       cctvID,
-			ParentName:     cctvEdited.Name,
-			Status:         "Edit",
-			Problem:        "Detail Cctv dirubah",
-			ProblemResolve: "",
-			CompleteStatus: enum.HInfo,
-			DateStart:      timeNow,
-			DateEnd:        timeNow,
-			Tag:            []string{},
-			Image:          "",
-		})
+	insertHistory := func() {
+		defer wg.Done()
+		//DB
+		_, err = c.daoH.InsertHistory(
+			dto.History{
+				ID:             primitive.NewObjectID(),
+				CreatedAt:      timeNow,
+				CreatedBy:      user.Name,
+				CreatedByID:    user.Identity,
+				UpdatedAt:      timeNow,
+				UpdatedBy:      user.Name,
+				UpdatedByID:    user.Identity,
+				Category:       category.Cctv,
+				Branch:         user.Branch,
+				ParentID:       cctvID,
+				ParentName:     cctvEdited.Name,
+				Status:         "Edit",
+				Problem:        "Detail Cctv dirubah",
+				ProblemResolve: "",
+				CompleteStatus: enum.HInfo,
+				DateStart:      timeNow,
+				DateEnd:        timeNow,
+				Tag:            []string{},
+				Image:          "",
+			})
+		errChan <- err
+	}
+
+	go editUnit()
+	go insertHistory()
+
+	go func() {
+		wg.Wait()
+		close(errChan)
+	}()
+
+	var errMessage string
+	for err := range errChan {
+		if err != nil {
+			errMessage = errMessage + ". "
+		}
+	}
+
+	if errMessage != "" {
+		errorPartial := errors.New("partial error")
+		restErr := rest_err.NewInternalServerError(fmt.Sprintf("Mengedit cctv berhasil namun : %s", errMessage), errorPartial)
+		logger.Error(restErr.Message(), errorPartial)
+		return nil, restErr
+	}
 
 	return cctvEdited, nil
 }
+
 func (c *cctvService) DeleteCctv(user mjwt.CustomClaim, id string) rest_err.APIError {
 
 	oid, errT := primitive.ObjectIDFromHex(id)
@@ -221,7 +299,6 @@ func (c *cctvService) DisableCctv(cctvID string, user mjwt.CustomClaim, value bo
 		return nil, rest_err.NewBadRequestError("ObjectID yang dimasukkan salah")
 	}
 
-	// IMPROVEMENT : Can use goroutine in next improvement
 	// set disable enable cctv
 	cctv, err := c.daoC.DisableCctv(oid, user, value)
 	if err != nil {
