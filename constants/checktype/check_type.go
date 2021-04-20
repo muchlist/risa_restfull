1b30f362acbdde4a5dc9c3077180f42d68af1288
muchlist <whois.muchlis@gmail.com>
2021-04-20 15:37:30 +0800

    update checkTypeAvailable

--- constants/checktype/check_type.go
+++ constants/checktype/check_type.go
@@ -1,11 +1,16 @@
 package checktype
 
 const (
-	Ups    = "UPS"
-	Server = "SERVER"
-	Cctv   = "CCTV"
+	Outstanding = "OUTSTANDING"
+	Koneksi     = "KONEKSI"
+	Gate        = "GATE"
+	Aplikasi    = "APLIKASI"
+	Ups         = "UPS"
+	Server      = "SERVER"
+	Improvement = "IMPROVEMENT"
+	Lainnya     = "LAINNYA"
 )
 
 func GetCheckTypeAvailable() []string {
-	return []string{Ups, Server, Cctv}
+	return []string{Outstanding, Koneksi, Gate, Aplikasi, Ups, Server, Improvement, Lainnya}
 }
