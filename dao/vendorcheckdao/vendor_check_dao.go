21b97a257504456d7364154e4dc8faef9d2cb853
muchlist <whois.muchlis@gmail.com>
2021-07-17 22:31:48 +0800

    vendor check dao

---
+++ dao/vendorcheckdao/vendor_check_dao.go
@@ -0,0 +1,294 @@
+package vendorcheckdao
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/muchlist/erru_utils_go/logger"
+	"github.com/muchlist/erru_utils_go/rest_err"
+	"github.com/muchlist/risa_restfull/db"
+	"github.com/muchlist/risa_restfull/dto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"strings"
+	"time"
+)
+
+const (
+	connectTimeout = 3
+	keyCollection  = "vendorCheck"
+	keyID          = "_id"
+	keyCreatedAt   = "created_at"
+	keyUpdatedAt   = "updated_at"
+	keyUpdatedBy   = "updated_by"
+	keyUpdatedByID = "updated_by_id"
+	keyBranch      = "branch"
+
+	keyTimeStarted      = "time_started"
+	keyTimeEnded        = "time_ended"
+	keyIsFinish         = "is_finish"
+	keyVendorCheckItems = "vendor_check_items"
+	keyNote             = "note"
+
+	keyChXId        = "vendor_check_items.id"
+	keyChXCheckedAt = "vendor_check_items.$.checked_at"
+	keyChXCheckedBy = "vendor_check_items.$.checked_by"
+	keyChXIsChecked = "vendor_check_items.$.is_checked"
+	keyChXIsBlur    = "vendor_check_items.$.is_blur"
+	keyChXIsOffline = "vendor_check_items.$.is_offline"
+	keyChXImagePath = "vendor_check_items.$.image_path"
+)
+
+func NewVendorCheckDao() CheckVendorDaoAssumer {
+	return &checkVendorDao{}
+}
+
+type checkVendorDao struct {
+}
+
+type CheckVendorDaoAssumer interface {
+	InsertCheck(input dto.VendorCheck) (*string, rest_err.APIError)
+	EditCheck(input dto.VendorCheckEdit) (*dto.VendorCheck, rest_err.APIError)
+	DeleteCheck(input dto.FilterIDBranchCreateGte) (*dto.VendorCheck, rest_err.APIError)
+	UploadChildImage(filterA dto.FilterParentIDChildIDAuthor, imagePath string) (*dto.VendorCheck, rest_err.APIError)
+	UpdateCheckItem(input dto.VendorCheckItemUpdate) (*dto.VendorCheck, rest_err.APIError)
+
+	GetCheckByID(checkID primitive.ObjectID, branchIfSpecific string) (*dto.VendorCheck, rest_err.APIError)
+	FindCheck(branch string, filterA dto.FilterTimeRangeLimit) ([]dto.VendorCheck, rest_err.APIError)
+}
+
+func (c checkVendorDao) InsertCheck(input dto.VendorCheck) (*string, rest_err.APIError) {
+	coll := db.DB.Collection(keyCollection)
+	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout*time.Second)
+	defer cancel()
+
+	// Default value for slice
+	input.Branch = strings.ToUpper(input.Branch)
+	if input.VendorCheckItems == nil {
+		input.VendorCheckItems = []dto.VendorCheckItemEmbed{}
+	}
+
+	result, err := coll.InsertOne(ctx, input)
+	if err != nil {
+		apiErr := rest_err.NewInternalServerError("Gagal menyimpan cctv check ke database", err)
+		logger.Error("Gagal menyimpan cctv check ke database, (VendorInsertCheck)", err)
+		return nil, apiErr
+	}
+
+	insertID := result.InsertedID.(primitive.ObjectID).Hex()
+
+	return &insertID, nil
+}
+
+func (c checkVendorDao) EditCheck(input dto.VendorCheckEdit) (*dto.VendorCheck, rest_err.APIError) {
+	coll := db.DB.Collection(keyCollection)
+	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout*time.Second)
+	defer cancel()
+
+	// Default value for slice
+
+	opts := options.FindOneAndUpdate()
+	opts.SetReturnDocument(1)
+
+	filter := bson.M{
+		keyID:       input.FilterID,
+		keyBranch:   input.FilterBranch,
+		keyIsFinish: false,
+	}
+
+	update := bson.M{
+		"$set": bson.M{
+			keyUpdatedAt:   input.UpdatedAt,
+			keyUpdatedBy:   input.UpdatedBy,
+			keyUpdatedByID: input.UpdatedByID,
+			keyTimeStarted: input.TimeStarted,
+			keyTimeEnded:   input.TimeEnded,
+			keyIsFinish:    input.IsFinish,
+			keyNote:        input.Note,
+		},
+	}
+
+	var check dto.VendorCheck
+	if err := coll.FindOneAndUpdate(ctx, filter, update, opts).Decode(&check); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, rest_err.NewBadRequestError("cctv check tidak diupdate : validasi id branch isFinish")
+		}
+
+		logger.Error("Gagal mendapatkan cctv check dari database (EditCheck)", err)
+		apiErr := rest_err.NewInternalServerError("Gagal mendapatkan cctv check dari database", err)
+		return nil, apiErr
+	}
+
+	return &check, nil
+}
+
+func (c checkVendorDao) DeleteCheck(input dto.FilterIDBranchCreateGte) (*dto.VendorCheck, rest_err.APIError) {
+	coll := db.DB.Collection(keyCollection)
+	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout*time.Second)
+	defer cancel()
+
+	filter := bson.M{
+		keyID:        input.FilterID,
+		keyBranch:    input.FilterBranch,
+		keyCreatedAt: bson.M{"$gte": input.FilterCreateGTE},
+	}
+
+	var check dto.VendorCheck
+	err := coll.FindOneAndDelete(ctx, filter).Decode(&check)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, rest_err.NewBadRequestError("Cctv Check tidak dihapus : validasi id branch time_reach")
+		}
+
+		logger.Error("Gagal menghapus cctvCheck dari database (VendorDeleteCheck)", err)
+		apiErr := rest_err.NewInternalServerError("Gagal menghapus cctv check dari database", err)
+		return nil, apiErr
+	}
+
+	return &check, nil
+}
+
+func (c checkVendorDao) UploadChildImage(filterA dto.FilterParentIDChildIDAuthor, imagePath string) (*dto.VendorCheck, rest_err.APIError) {
+	coll := db.DB.Collection(keyCollection)
+	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout*time.Second)
+	defer cancel()
+
+	opts := options.FindOneAndUpdate()
+	opts.SetReturnDocument(1)
+
+	filter := bson.M{
+		keyID:    filterA.FilterParentID,
+		keyChXId: filterA.FilterChildID,
+	}
+
+	update := bson.M{
+		"$set": bson.M{
+			keyChXImagePath: imagePath,
+		},
+	}
+
+	var check dto.VendorCheck
+	if err := coll.FindOneAndUpdate(ctx, filter, update, opts).Decode(&check); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, rest_err.NewBadRequestError(fmt.Sprintf("Memasukkan path image gagal, cctv check dengan id %s -> %s tidak ditemukan", filterA.FilterParentID.Hex(), filterA.FilterChildID))
+		}
+
+		logger.Error("Memasukkan path image child cctv Check ke db gagal, (VendorUploadImage)", err)
+		apiErr := rest_err.NewInternalServerError("Memasukkan path image cctv check ke db gagal", err)
+		return nil, apiErr
+	}
+
+	return &check, nil
+}
+
+func (c checkVendorDao) UpdateCheckItem(input dto.VendorCheckItemUpdate) (*dto.VendorCheck, rest_err.APIError) {
+	coll := db.DB.Collection(keyCollection)
+	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout*time.Second)
+	defer cancel()
+
+	opts := options.FindOneAndUpdate()
+	opts.SetReturnDocument(1)
+
+	filter := bson.M{
+		keyID:       input.FilterParentID,
+		keyChXId:    input.FilterChildID,
+		keyIsFinish: false,
+	}
+
+	update := bson.M{
+		"$set": bson.M{
+			keyChXCheckedAt: input.CheckedAt,
+			keyChXCheckedBy: input.CheckedBy,
+			keyChXIsChecked: input.IsChecked,
+			keyChXIsBlur:    input.IsBlur,
+			keyChXIsOffline: input.IsOffline,
+		},
+	}
+
+	var check dto.VendorCheck
+	if err := coll.FindOneAndUpdate(ctx, filter, update, opts).Decode(&check); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, rest_err.NewBadRequestError("cctv check tidak diupdate : validasi id branch isFinish")
+		}
+
+		logger.Error("Gagal mendapatkan checkItem dari database (VendorUpdateCheckItem)", err)
+		apiErr := rest_err.NewInternalServerError("Gagal mendapatkan cctv check dari database", err)
+		return nil, apiErr
+	}
+
+	return &check, nil
+}
+
+func (c checkVendorDao) GetCheckByID(checkID primitive.ObjectID, branchIfSpecific string) (*dto.VendorCheck, rest_err.APIError) {
+	coll := db.DB.Collection(keyCollection)
+	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout*time.Second)
+	defer cancel()
+
+	filter := bson.M{keyID: checkID}
+	// filter condition
+	if branchIfSpecific != "" {
+		filter[keyBranch] = strings.ToUpper(branchIfSpecific)
+	}
+
+	var check dto.VendorCheck
+	if err := coll.FindOne(ctx, filter).Decode(&check); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			apiErr := rest_err.NewNotFoundError(fmt.Sprintf("cctv check dengan ID %s tidak ditemukan. validation : id branch", checkID.Hex()))
+			return nil, apiErr
+		}
+
+		logger.Error("gagal mendapatkan cctv check dari database (GetCheckByID)", err)
+		apiErr := rest_err.NewInternalServerError("Gagal mendapatkan cctv check dari database", err)
+		return nil, apiErr
+	}
+
+	return &check, nil
+}
+
+func (c checkVendorDao) FindCheck(branch string, filterA dto.FilterTimeRangeLimit) ([]dto.VendorCheck, rest_err.APIError) {
+	coll := db.DB.Collection(keyCollection)
+	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout*time.Second)
+	defer cancel()
+
+	if filterA.Limit == 0 {
+		filterA.Limit = 100
+	}
+
+	// filter
+	filter := bson.M{}
+	// filter condition
+	if branch != "" {
+		filter[keyBranch] = strings.ToUpper(branch)
+	}
+	if filterA.FilterStart != 0 {
+		filter[keyCreatedAt] = bson.M{"$gte": filterA.FilterStart}
+	}
+	if filterA.FilterEnd != 0 {
+		filter[keyCreatedAt] = bson.M{"$lte": filterA.FilterEnd}
+	}
+
+	opts := options.Find()
+	opts.SetProjection(bson.M{
+		keyVendorCheckItems: 0,
+	})
+	opts.SetSort(bson.D{{keyID, -1}}) //nolint:govet
+	opts.SetLimit(filterA.Limit)
+
+	cursor, err := coll.Find(ctx, filter, opts)
+	if err != nil {
+		logger.Error("gagal mendapatkan daftar cctv check dari database (FindCheck)", err)
+		apiErr := rest_err.NewInternalServerError("Database error", err)
+		return []dto.VendorCheck{}, apiErr
+	}
+
+	var checkList []dto.VendorCheck
+	if err = cursor.All(ctx, &checkList); err != nil {
+		logger.Error("Gagal decode checkList cursor ke objek slice (VendorFindCheck)", err)
+		apiErr := rest_err.NewInternalServerError("Database error", err)
+		return []dto.VendorCheck{}, apiErr
+	}
+
+	return checkList, nil
+}
--- dto/vendor_check_dto.go
+++ dto/vendor_check_dto.go
@@ -3,16 +3,19 @@ package dto
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type VendorCheck struct {
-	ID          primitive.ObjectID     `json:"id,omitempty" bson:"_id,omitempty"`
-	CreatedAt   int64                  `json:"created_at" bson:"created_at"`
-	CreatedBy   string                 `json:"created_by" bson:"created_by"`
-	CreatedByID string                 `json:"created_by_id" bson:"created_by_id"`
-	Branch      string                 `json:"branch" bson:"branch"`
-	TimeStarted int64                  `json:"time_started" bson:"time_started"`
-	TimeEnded   int64                  `json:"time_ended" bson:"time_ended"`
-	IsFinish    bool                   `json:"is_finish" bson:"is_finish"`
-	CheckItems  []VendorCheckItemEmbed `json:"check_items" bson:"check_items"`
-	Note        string                 `json:"note" bson:"note"`
+	ID               primitive.ObjectID     `json:"id,omitempty" bson:"_id,omitempty"`
+	CreatedAt        int64                  `json:"created_at" bson:"created_at"`
+	CreatedBy        string                 `json:"created_by" bson:"created_by"`
+	CreatedByID      string                 `json:"created_by_id" bson:"created_by_id"`
+	UpdatedAt        int64                  `json:"updated_at" bson:"updated_at"`
+	UpdatedBy        string                 `json:"updated_by" bson:"updated_by"`
+	UpdatedByID      string                 `json:"updated_by_id" bson:"updated_by_id"`
+	Branch           string                 `json:"branch" bson:"branch"`
+	TimeStarted      int64                  `json:"time_started" bson:"time_started"`
+	TimeEnded        int64                  `json:"time_ended" bson:"time_ended"`
+	IsFinish         bool                   `json:"is_finish" bson:"is_finish"`
+	VendorCheckItems []VendorCheckItemEmbed `json:"vendor_check_items" bson:"vendor_check_items"`
+	Note             string                 `json:"note" bson:"note"`
 }
 
 type VendorCheckItemEmbed struct {
@@ -27,10 +30,30 @@ type VendorCheckItemEmbed struct {
 	IsOffline bool   `json:"is_offline" bson:"is_offline"`
 }
 
+type VendorCheckEdit struct {
+	FilterIDBranch
+	UpdatedAt   int64
+	UpdatedBy   string
+	UpdatedByID string
+	TimeStarted int64
+	TimeEnded   int64
+	IsFinish    bool
+	Note        string
+}
+
 type VendorCheckItemUpdateRequest struct {
 	ParentID  string `json:"parent_id"`
 	ChildID   string `json:"child_id"`
 	IsChecked bool   `json:"is_checked"`
 	IsBlur    bool   `json:"is_blur"`
 	IsOffline bool   `json:"is_offline"`
+}
+
+type VendorCheckItemUpdate struct {
+	FilterParentIDChildIDAuthor
+	CheckedAt int64
+	CheckedBy string
+	IsChecked bool
+	IsBlur    bool
+	IsOffline bool
 }
